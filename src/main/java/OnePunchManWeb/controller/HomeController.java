e8158fe211f1f239d5cfff77cc90e473a6f44283
SteveenDomingue <steveena.dominguezb@utadeo.edu.co>
2020-05-24 12:33:43 -0500

    empezando proyecto

---
+++ src/main/java/OnePunchManWeb/controller/HomeController.java
@@ -0,0 +1,24 @@
+package OnePunchManWeb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping(value= "/", method = RequestMethod.GET)
+	public String HomePage() {
+		return "home";
+	}
+	
+}
